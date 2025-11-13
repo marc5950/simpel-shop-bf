3aee33cedb7c25c99d2c4ac2a50df6dd12652668
marc5950 <marcuswagner5950@gmail.com>
2025-11-13 12:25:35 +0100

    added hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,20 @@
 import Btn from "./Btn";
+import Image from "next/image";
+import HeroImg from "../../public/hero.jpg";
 
 const Hero = () => {
   return (
-    <section>
-      Hero component
-      <h1>Simpel Shop</h1>
-      <Btn text="Se Produkter" />
+    <section className="-mt-[103px] grid max-h-full">
+      <Image
+        src={HeroImg}
+        alt="Hero Image"
+        loading="eager"
+        className="col-1 row-1 h-full"
+      />
+      <div className="bg-bg2 col-1 row-1 m-24 place-self-start self-end rounded p-6">
+        <h1 className="">Simpel Shop</h1>
+        <Btn text="Se Produkter" />
+      </div>
     </section>
   );
 };
