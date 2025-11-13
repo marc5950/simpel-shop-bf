1c370f2f598d2cef376f19e811ceedadb0e4e6f5
marc5950 <marcuswagner5950@gmail.com>
2025-11-13 12:52:09 +0100

    refactor: update button component and improve hero section layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,22 @@
 import Btn from "./Btn";
 import Image from "next/image";
 import HeroImg from "../../public/hero.jpg";
+import Link from "next/link";
 
 const Hero = () => {
   return (
-    <section className="-mt-[103px] grid max-h-full">
+    <section className="-mt-[104px] grid max-h-full">
       <Image
         src={HeroImg}
         alt="Hero Image"
         loading="eager"
-        className="col-1 row-1 h-full"
+        className="col-1 row-1 h-screen w-screen object-cover"
       />
-      <div className="bg-bg2 col-1 row-1 m-24 place-self-start self-end rounded p-6">
-        <h1 className="">Simpel Shop</h1>
-        <Btn text="Se Produkter" />
+      <div className="col-1 row-1 m-[15%] flex flex-col gap-6 place-self-start self-end rounded bg-white/50 p-12 backdrop-blur-sm">
+        <h1 className="font-title text-5xl font-bold">Simpel Shop</h1>
+        <Link href="/products">
+          <Btn text="Se Produkter" type="normal" />
+        </Link>
       </div>
     </section>
   );
