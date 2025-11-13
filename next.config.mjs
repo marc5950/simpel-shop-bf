1d8869ba103c4be26a4da508843a9c1b08d64633
Aylin Saber <saberaylin@gmail.com>
2025-11-13 22:54:29 +0100

    added first iteration of Card and CardList

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
